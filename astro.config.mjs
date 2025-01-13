bb9057dd940735a6a00a097f22e6706e1ed72c29
José Calderón - Duoc Account <tutuyosoy1111@gmail.com>
2025-01-13 18:54:15 -0300

    intento 2
    intento

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://josecalderon7.github.io',
-  base: 'porfolio-2025',
+  base: '/porfolio-2025',
+  trailingSlash: 'always',
   integrations: [tailwind()]
 });
